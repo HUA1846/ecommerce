716c71444b52223c8a9377a2211ddb6e91dfdfdc
HUA1846 <hua1378246@gmail.com>
2021-05-12 22:44:33 -0700

    add page size selector

--- frontend/ecommerce/src/app/components/product-list/product-list.component.ts
+++ frontend/ecommerce/src/app/components/product-list/product-list.component.ts
@@ -18,7 +18,7 @@ export class ProductListComponent implements OnInit {
 
   // properties for pagination
   thePageNumber: number = 1;
-	thePageSize: number = 10;
+	thePageSize: number = 5;
   theTotalElements: number = 0;
 
 
@@ -102,5 +102,11 @@ export class ProductListComponent implements OnInit {
         this.thePageSize = data.page.size;
         this.theTotalElements = data.page.totalElements;
       }
+    }
+
+    updatePageSize(value: number) {
+        this.thePageSize = value;
+        this.thePageNumber = 1;
+        this.listProducts();
     }
 }
