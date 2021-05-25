8aa8c837fead2a760898cfd780d5640a9c3ec39a
HUA1846 <hua1378246@gmail.com>
2021-05-24 18:53:53 -0700

    complete customer checkout form

--- frontend/ecommerce/src/app/components/checkout/checkout.component.ts
+++ frontend/ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -8,18 +8,18 @@ import { FormGroup } from '@angular/forms';
 })
 export class CheckoutComponent implements OnInit {
 
-  checkcoutFormGroup: FormGroup;
+  checkoutFormGroup: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-    // this.checkcoutFormGroup = this.formBuilder.group({
-    //     customer: this.formBuilder.group({
-    //       firstName: [''],
-    //       lastName: [''],
-    //       email: ['']
-    //     })
-    // });
+    this.checkoutFormGroup = this.formBuilder.group({
+        customer: this.formBuilder.group({
+          firstName: [''],
+          lastName: [''],
+          email: ['']
+        })
+    });
   }
 
   
