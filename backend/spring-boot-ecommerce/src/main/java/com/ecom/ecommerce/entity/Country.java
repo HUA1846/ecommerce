16ca24bac484aedeab663ae180be1bc020c66594
HUA1846 <hua1378246@gmail.com>
2021-05-31 14:26:56 -0700

    create countryRepository

--- backend/spring-boot-ecommerce/src/main/java/com/ecom/ecommerce/entity/Country.java
+++ backend/spring-boot-ecommerce/src/main/java/com/ecom/ecommerce/entity/Country.java
@@ -1,9 +1,11 @@
 package com.ecom.ecommerce.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name="country")
@@ -21,4 +23,8 @@ public class Country {
 
     @Column(name="name")
     private String name;
+
+    @OneToMany(mappedBy = "country")
+    @JsonIgnore
+    private List<State> states;
 }
