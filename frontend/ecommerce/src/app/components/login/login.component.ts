b7d86cf2658069c77a3d75b37dad170558090dcc
unknown <hua1378246@gmail.com>
2022-02-08 20:22:12 -0800

    implement okta

---
+++ frontend/ecommerce/src/app/components/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { OktaAuthStateService } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+import * as OktaSignIn from '@okta/okta-signin-widget'
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  oktaSignin: any;
+
+  constructor(private oktaAuthService: OktaAuthStateService) { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ frontend/ecommerce/src/app/config/app-config.ts
@@ -0,0 +1,9 @@
+export default {
+    oidc: {
+        clientId: '0oafhsj30KySIC4Hj696',
+        issuer: 'https://u-northwesternelee.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
+
